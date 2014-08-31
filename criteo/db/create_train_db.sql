5079aef500f060ba10ad4d508afe2990892f969e
bchnge <ben.y.cheng@gmail.com>
2014-08-30 20:12:02 -0400

    update

--- criteo/db/create_train_db.sql
+++ criteo/db/create_train_db.sql
@@ -1,17 +1,17 @@
 .separator ","
 
 create table test (Id text primary key, I1 integer, I2 integer, I3 integer, I4 integer, I5 integer, I6 integer, I7 integer, I8 integer, I9 integer, I10 integer, I11 integer, I12 integer, I13 integer, C1 text, C2 text, C3 text, C4 text, C5 text, C6 text, C7 text, C8 text, C9 text, C10 text, C11 text, C12 text, C13 text, C14 text, C15 text, C16 text, C17 text, C18 text, C19 text, C20 text, C21 text, C22 text, C23 text, C24 text, C25 text, C26 text);
-.import data/test.csv test
+.import ../data/test.csv test
 
 create table train (Id text primary key, Label integer, I1 integer, I2 integer, I3 integer, I4 integer, I5 integer, I6 integer, I7 integer, I8 integer, I9 integer, I10 integer, I11 integer, I12 integer, I13 integer, C1 text, C2 text, C3 text, C4 text, C5 text, C6 text, C7 text, C8 text, C9 text, C10 text, C11 text, C12 text, C13 text, C14 text, C15 text, C16 text, C17 text, C18 text, C19 text, C20 text, C21 text, C22 text, C23 text, C24 text, C25 text, C26 text);
-.import data/train.csv train
+.import ../data/train.csv train
 
-# Split training data into positive and negative datasets for balanced sampling
+-- Split training data into positive and negative datasets for balanced sampling
 create table train_pos (Id text primary key, Label integer, I1 integer, I2 integer, I3 integer, I4 integer, I5 integer, I6 integer, I7 integer, I8 integer, I9 integer, I10 integer, I11 integer, I12 integer, I13 integer, C1 text, C2 text, C3 text, C4 text, C5 text, C6 text, C7 text, C8 text, C9 text, C10 text, C11 text, C12 text, C13 text, C14 text, C15 text, C16 text, C17 text, C18 text, C19 text, C20 text, C21 text, C22 text, C23 text, C24 text, C25 text, C26 text);
 insert into train_pos 
 	select * from train where Label = 1;
 
 create table train_neg (Id text primary key, Label integer, I1 integer, I2 integer, I3 integer, I4 integer, I5 integer, I6 integer, I7 integer, I8 integer, I9 integer, I10 integer, I11 integer, I12 integer, I13 integer, C1 text, C2 text, C3 text, C4 text, C5 text, C6 text, C7 text, C8 text, C9 text, C10 text, C11 text, C12 text, C13 text, C14 text, C15 text, C16 text, C17 text, C18 text, C19 text, C20 text, C21 text, C22 text, C23 text, C24 text, C25 text, C26 text);
+
 insert into train_neg
 	select * from train where Label = 0;
-
--- criteo/scripts/extract_categorical.sh
+++ criteo/scripts/extract_categorical.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# array=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26)
-array=(13 14 15 16 17 18)
+ array=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26)
+
 for I in "${array[@]}"
 do
-(sqlite3 advertising.db "DROP TABLE ct_c$I; CREATE TABLE ct_c$I (c$I text, Label integer, N integer); INSERT INTO ct_c$I SELECT c$I, Label, count(Id) as N FROM train group by Label,C$I;") 
+(sqlite3 advertising.db "CREATE TABLE ct_c$I (c$I text, Label integer, N integer); INSERT INTO ct_c$I SELECT c$I, Label, count(Id) as N FROM train group by Label,C$I;") 
 done
